2367312e716e851dc74150de0fd64dc3cb9275fb
David Jones <dave.jones@hmcts.net>
2023-08-14 11:40:46 +0100

    Update group access to dcd_group_bar_v2

--- key-vault.tf
+++ key-vault.tf
@@ -7,7 +7,7 @@ module "bar-vault" {
   object_id = var.jenkins_AAD_objectId
   resource_group_name = azurerm_resource_group.rg.name
   # group id of dcd_reform_dev_azure
-  product_group_object_id = "56679aaa-b343-472a-bb46-58bbbfde9c3d"
+  product_group_object_id = "637c823a-d4d6-4cac-bb6c-dd8159309e0d"
   common_tags         = var.common_tags
   managed_identity_object_ids = ["${var.managed_identity_object_id}"]
   create_managed_identity = true
