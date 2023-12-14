2de905bbb42a2e08d3576c97327c67b41fcfe986
Jordan Hoey <jordan.hoey@justice.gov.uk>
2023-12-14 12:26:06 +0000

    DTSPO-15909 - migrated Application Insights

--- application-insights.tf
+++ application-insights.tf
@@ -13,6 +13,23 @@ resource "azurerm_application_insights" "appinsights" {
     }
 }
 
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env     = var.env
+  product = var.product
+  name    = "${var.product}"
+
+  resource_group_name = azurerm_resource_group.rg.name
+
+  common_tags = var.common_tags
+}
+
+moved {
+  from = azurerm_application_insights.appinsights
+  to   = module.application_insights.azurerm_application_insights.this
+}
+
 # output "appInsightsInstrumentationKey" {
 #   value = "${azurerm_application_insights.appinsights.instrumentation_key}"
 # }
